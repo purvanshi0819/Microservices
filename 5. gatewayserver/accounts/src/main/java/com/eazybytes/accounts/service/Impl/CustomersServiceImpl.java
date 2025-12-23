911b3b544722b9145a5e8455748bb8b52b024d1f
Purvanshi Patidar <purvanshipatidar19@gmail.com>
2025-12-23 15:12:58 +0530

    Edge Server using Spring Cloud Gateway

---
+++ 5. gatewayserver/accounts/src/main/java/com/eazybytes/accounts/service/Impl/CustomersServiceImpl.java
@@ -0,0 +1,45 @@
+package com.eazybytes.accounts.service.Impl;
+
+import com.eazybytes.accounts.dto.*;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exception.ResourceNotFoundException;
+import com.eazybytes.accounts.mapper.AccountsMapper;
+import com.eazybytes.accounts.mapper.CustomerMapper;
+import com.eazybytes.accounts.repository.AccountsRepository;
+import com.eazybytes.accounts.repository.CustomerRepository;
+import com.eazybytes.accounts.service.ICustomersService;
+import com.eazybytes.accounts.service.client.CardsFeignClient;
+import com.eazybytes.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private LoansFeignClient loansFeignClient;
+    private CardsFeignClient cardsFeignClient;
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+        Customer customer=customerRepository.findByMobileNumber(mobileNumber).orElseThrow(()-> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber));
+        CustomerDto customerDto= CustomerMapper.maptoCustomerDto(customer, new CustomerDto());
+
+        Accounts accounts=accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(()-> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString()));
+        AccountsDto accountsDto= AccountsMapper.maptoAccountsDto(accounts, new AccountsDto());
+        CustomerDetailsDto customerDetailsDto=CustomerMapper.maptoCustomerDetailsDto(customer, new CustomerDetailsDto());
+
+        customerDetailsDto.setAccountsDto(AccountsMapper.maptoAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity=loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity=cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+    }
+}
