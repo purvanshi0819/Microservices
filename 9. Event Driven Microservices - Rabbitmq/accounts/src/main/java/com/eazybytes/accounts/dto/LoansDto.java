6746e699996dfe91ba783f0e798db2f2fb752590
Purvanshi Patidar <purvanshipatidar19@gmail.com>
2026-01-14 00:08:36 +0530

    Event Driven Microservices using RabbitMQ, Spring Cloud Functions and Stream

---
+++ 9. Event Driven Microservices - Rabbitmq/accounts/src/main/java/com/eazybytes/accounts/dto/LoansDto.java
@@ -0,0 +1,54 @@
+package com.eazybytes.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Data;
+
+@Schema(
+        name = "Loans",
+        description = "Schema to hold loan information"
+)
+@Data
+public class LoansDto {
+
+    @Schema(
+            name = "Mobile Number of Customer", example = "68769886877"
+    )
+    @NotEmpty(message = "Mobile number cannot be null and empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile Number must be 10 digits")
+    private String mobileNumber;
+
+    @Schema(
+            name = "Loan Number of Customer", example = "548732457654"
+    )
+    @NotEmpty(message = "Loan Number can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{12})",message = "LoanNumber must be 12 digits")
+    private String loanNumber;
+
+    @NotEmpty(message = "LoanType can not be a null or empty")
+    @Schema(
+            description = "Type of the loan", example = "Home Loan"
+    )
+    private String loanType;
+
+    @Positive(message = "Total loan amount should be greater than zero")
+    @Schema(
+            description = "Total loan amount", example = "100000"
+    )
+    private int totalLoan;
+
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than zero")
+    @Schema(
+            description = "Total loan amount paid", example = "1000"
+    )
+    private int amountPaid;
+
+    @PositiveOrZero(message = "Total outstanding amount should be equal or greater than zero")
+    @Schema(
+            description = "Total outstanding amount against a loan", example = "99000"
+    )
+    private int outstandingAmount;
+}
