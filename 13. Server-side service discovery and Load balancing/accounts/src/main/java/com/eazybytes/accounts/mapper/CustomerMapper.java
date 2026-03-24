80fc03f9f4ab037d22fe5ad010e7fa64c45637df
Purvanshi Patidar <purvanshipatidar19@gmail.com>
2026-03-24 11:19:00 +0530

    Setup k8s discovery server and service discovery client

---
+++ 13. Server-side service discovery and Load balancing/accounts/src/main/java/com/eazybytes/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,29 @@
+package com.eazybytes.accounts.mapper;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+
+public class CustomerMapper {
+
+    public static CustomerDto maptoCustomerDto(Customer customer, CustomerDto customerDto){
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static CustomerDetailsDto maptoCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto){
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
+    }
+
+    public  static Customer maptoCustomer(CustomerDto customerDto, Customer customer){
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
---
+++ 13. Server-side service discovery and Load balancing/message/src/main/java/com/eazybytes/message/dto/AccountsMsgDto.java
@@ -0,0 +1,9 @@
+package com.eazybytes.message.dto;
+/**
+ * @param accountNumber
+ * @param name
+ * @param email
+ * @param mobileNumber
+ */
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+}
